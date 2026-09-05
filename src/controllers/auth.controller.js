7dabb13cc40f9343413c8faf251b6e019e767b56
Rajatraiiii <rairajat280@gmail.com>
2026-09-05 16:18:27 +0530

    Add user registration authentication

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,50 @@
+const userModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+
+/*
+* user register controller
+* POST /api/auth/register
+*/
+async function userRegisterController(req,res){
+    const {email,name,password} = req.body;
+
+    const isExist = await userModel.findOne({
+        email:email
+    })
+
+    if(isExist){
+        return res.status(422).json({
+            success:false,
+            message:"Email already exists",
+            status: "failed"
+        })
+    }
+
+    const user = await userModel.create({
+        email,
+        name,
+        password
+    })
+
+    const token = jwt.sign({userId:user._id},process.env.JWT_SECRET, { expiresIn: '3d' });
+
+    res.cookie('token', token)
+    res.status(201).json({
+        user:{
+            _id:user._id,
+            email:user.email,
+            name:user.name
+        },
+        token:token,
+        success:true,
+        message:"User registered successfully",
+        status:"success"
+        })
+    
+
+
+}
+
+module.exports = {
+    userRegisterController
+}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -20,6 +23,29 @@ const userSchema = new mongoose.Schema({
         required: [true,"Password is required"],
         minlength: [6,"Password must be at least 6 characters long"],
         select: false
-        
-    },
+
+    }
+},{
+    timestamps: true
 });
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        return next();
+    }
+
+    const hash = await bcrypt.hash(this.password,10);
+    this.password = hash;
+    return next()
+
+
+
+})
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password,this.password);
+
+}
+
+const userModel = mongoose.model('User',userSchema);
+module.exports = userModel;
