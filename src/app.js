d232bc12758fae4d968cc24f3b9345619dd7e3a1
Rajatraiiii <rairajat280@gmail.com>
2026-09-10 23:15:20 +0530

    Update app startup and ignore env files

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ const authRouter = require('./routes/auth.routes');
 const accountRouter = require('./routes/account.routes');
 const transactionRouter = require('./routes/transaction.routes');
 
+
+app.get("/",(req,res) => {
+    res.send("BANKING SERVICE IS UP AND RUNNING")
+})
 app.use('/api/auth', authRouter);
 app.use('/api/accounts', accountRouter);
 app.use('/api/transactions', transactionRouter);
